3fd9bb901364ab4ce79b24122b593b608880d663
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-11 02:18:07 -0800

    Commiting changes

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate random number
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log('dice', dice);
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  // 3. Check for rolled 1: If true move to the next player.
+});

73e68a31283c7d246429e292746954af2c037a3a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-12-11 18:29:57 -0800

    Commiting changes

--- script.js
+++ script.js
@@ -11,15 +11,22 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  // Starting conditions
+  const scores = [0, 0];
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
+  score1El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player-active');
+  player1El.classList.remove('player-active');
+};
 
 const switchPlayer = function () {
   // Switch to the next player
@@ -81,13 +88,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  score1El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player-active');
-  player1El.classList.remove('player-active');
-});
+btnNew.addEventListener('click', function () {});
